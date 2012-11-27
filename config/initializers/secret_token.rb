daa1af429501f85cbec858855827bf2a6b4774fb
gelegele <ryo@ryo-macbook.local>
2012-11-27 18:26:27 +0900

    rails new railsauth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsauth::Application.config.secret_token = 'f5442cd937a04716789e69770993d11a996af2ca90cfe9db4319f0a7189f6000f2af15a868f3301710e9c36253788cd745ea27110a23bff8a0f7a9739a639b7e'
