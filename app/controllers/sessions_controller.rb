24ce14e00e397e405bdb21ca5e594375e9bbe593
gelegele <ryo@ryo-macbook.local>
2012-11-27 21:22:27 +0900

    ログイン画面完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      #redirect_to root_url, :notice => "Logged in!"
+      render :text => "Logged in!"
+    else
+      render "new"
+    end
+  end
+
 end
