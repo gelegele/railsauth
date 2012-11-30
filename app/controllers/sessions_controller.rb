81b5d59987ecdde9114ff5a321a782335d9766d5
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2012-11-30 13:18:09 +0900

    users index 作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      #redirect_to root_url, :notice => "Logged in!"
-      render :text => "Logged in!"
+      redirect_to users_url, :notice => "Logged in!"
     else
       render "new"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      render :text => "Signed Up!"
+      redirect_to users_url, :notice => "Signed Up!"
     else
       render "new"
     end
+  end
+
+  def index
+    @users = User.all
   end
 end
