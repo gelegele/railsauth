aa98ae957c0d82fde9f838285b5a7956425719e0
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2012-12-05 19:23:59 +0900

    ログアウト機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,8 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Loged out!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Railsauth::Application.routes.draw do
-  get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
 
-  root :to => "sessions#new"
   resources :users, :sessions
 
   # The priority is based upon order of creation:
@@ -55,6 +55,7 @@ Railsauth::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
